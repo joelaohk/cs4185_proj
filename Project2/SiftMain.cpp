1f6b17b8b1e19d1cd61f23fee25f51fddbdf8da5
DESKTOP-06J1EDP\Joe <joelao@icloud.com>
2017-11-20 00:39:36 +0800

    added comments

--- Project2/SiftMain.cpp
+++ Project2/SiftMain.cpp
@@ -1,3 +1,22 @@
+/*
+*	SiftMain.cpp 
+*	Experimental file for CS4185 Project
+*	Created by LAO Choi Hin (54045244)
+*
+*/
+
+/************************************************************************************************************
+*	To Markers:																								*
+*	1.	This file is created just for experiment,															*
+*		it should not be considered as part of the project submission.										*
+*		But you may use it to see how I came up with the approaches used in the official submission.		*
+*																											*
+*	2.	This file may contains code from other sources and eventually applied in the official submission.	*
+*		Refereces to those sources have been made in the report.											*
+*																											*
+*	Thank you for your kind attention and understanding.													*
+************************************************************************************************************/
+
 #include <iostream>
 #include <stdio.h>
 #include <opencv2/legacy/legacy.hpp>
